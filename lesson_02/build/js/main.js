d8e4bc4be3af6242a855cafe664e3316455f08ab
vankuver163 <de1uxe87@mail.ru>
2024-01-06 16:26:32 +0400

    lesson_02

---
+++ lesson_02/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let myName = "pavel";
+let meaningOfLife;
+let isLoading;
+let album;
+myName = "Pavel";
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
