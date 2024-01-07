9b6f240e0de36be4c8f358869cf4b8e8d534194c
vankuver163 <de1uxe87@mail.ru>
2024-01-07 12:49:38 +0400

    lesson_07

---
+++ lesson_07/build/js/main.js
@@ -0,0 +1,43 @@
+"use strict";
+// interface TransactionObj {
+//   [index: string]: number
+// }
+const todaysTransactions = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50,
+};
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions["Pizza"]);
+let prop = "Pizza";
+console.log(todaysTransactions[prop]);
+const todaysNet = (transactions) => {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[transaction];
+    }
+    return total;
+};
+console.log(todaysNet(todaysTransactions));
+const student = {
+    name: "Doug",
+    GPA: 3.5,
+    classes: [100, 200],
+};
+//console.log(student.test)
+for (const key in student) {
+    console.log(`${key}: ${student[key]}`);
+}
+Object.keys(student).map((key) => console.log(student[key]));
+const logStudentKey = (student, key) => {
+    console.log(`Student ${key}: ${student[key]}`);
+};
+logStudentKey(student, "GPA");
+const monthlyIncomes = {
+    salary: 500,
+    bonus: 100,
+    sidehustle: 250,
+};
+for (const revenue in monthlyIncomes) {
+    console.log(monthlyIncomes[revenue]);
+}
