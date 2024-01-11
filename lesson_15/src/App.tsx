66a02b73c77e096eeac11e955d473de9624b8ea8
vankuver163 <de1uxe87@mail.ru>
2024-01-11 12:54:00 +0400

    lesson_15 - ProductList

--- lesson_15/src/App.tsx
+++ lesson_15/src/App.tsx
@@ -1,11 +1,23 @@
-
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Cart from "./components/Cart";
+import ProductList from "./components/ProductList";
+import { useState } from "react";
 
 function App() {
-  return (   
-      <div>
-    
-      </div>  
-  )
+  const [viewCart, setViewCart] = useState<boolean>(false);
+
+  const pageContent = viewCart ? <Cart /> : <ProductList />;
+
+  const content = (
+    <>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      {pageContent}
+      <Footer viewCart={viewCart} />
+    </>
+  );
+
+  return content;
 }
 
-export default App
+export default App;
