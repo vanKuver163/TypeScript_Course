2254d8864218f8c7f1bb7f1b404efe388e9fd892
vankuver163 <de1uxe87@mail.ru>
2024-01-12 10:07:40 +0400

    lesson_15 - done

--- lesson_15/src/components/CartLineItem.tsx
+++ lesson_15/src/components/CartLineItem.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, ReactElement } from "react";
+import { ChangeEvent, ReactElement, memo } from "react";
 import { CartItemType } from "../context/CartProvider";
 import { ReducerAction } from "../context/CartProvider";
 import { ReducerActionType } from "../context/CartProvider";
@@ -87,5 +87,15 @@ const CartLineItem = ({ item, dispatch, REDUCER_ACTIONS }: PropsType) => {
 
   return content
 };
+
+function areItemsEqual({item: prevItem}: PropsType, {item: nextItem}: PropsType){
+  return Object.keys(prevItem).every(key => {
+    return prevItem[key as keyof CartItemType] === nextItem[key as keyof CartItemType]
+  }) 
+}
+
+const MemoizedCartLineItem = memo<typeof CartLineItem>(CartLineItem, areItemsEqual)
+
+
 
 export default CartLineItem;
--- lesson_15/src/components/Product.tsx
+++ lesson_15/src/components/Product.tsx
@@ -1,6 +1,6 @@
 import { ProductType } from "../context/ProductsProvider";
 import { ReducerActionType, ReducerAction } from "../context/CartProvider";
-import { ReactElement } from "react";
+import { ReactElement, memo } from "react";
 
 type PropsType = {
   product: ProductType;
@@ -9,24 +9,46 @@ type PropsType = {
   inCart: boolean;
 };
 
-const Product = ({product, dispatch, REDUCER_ACTIONS, inCart}: PropsType): ReactElement => {
+const Product = ({
+  product,
+  dispatch,
+  REDUCER_ACTIONS,
+  inCart,
+}: PropsType): ReactElement => {
+  const img: string = new URL(`../images/${product.sku}.jpg`, import.meta.url)
+    .href;
 
-const img: string = new URL(`../images/${product.sku}.jpg`, import.meta.url).href
+  const onAddToCart = () =>
+    dispatch({ type: REDUCER_ACTIONS.ADD, payload: { ...product, qty: 1 } });
 
-const onAddToCart = () => dispatch({type: REDUCER_ACTIONS.ADD, payload: {...product, qty: 1}})
+  const itemInCart = inCart ? " → Item in Cart: ✔️" : null;
 
-const itemInCart = inCart ? ' → Item in Cart: ✔️' : null
-
-const content = (
+  const content = (
     <article className="product">
-        <h3>{product.name}</h3>
-        <img src={img} alt={product.name} className="product__img"/>
-        <p>{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(product.price)}{itemInCart}</p>
-        <button onClick={onAddToCart}>Add to Cart</button>
+      <h3>{product.name}</h3>
+      <img src={img} alt={product.name} className="product__img" />
+      <p>
+        {new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(product.price)}
+        {itemInCart}
+      </p>
+      <button onClick={onAddToCart}>Add to Cart</button>
     </article>
-)
+  );
 
-return content
+  return content;
 };
+
+function areProductsEqual(
+  { product: prevProduct, inCart: prevInCart }: PropsType,
+  { product: nextProduct, inCart: nextInCart }: PropsType
+) {
+  return Object.keys(prevProduct).every((key) => {
+    return prevProduct[key as keyof ProductType] === nextProduct[key as keyof ProductType]}) && prevInCart === nextInCart 
+}
+
+const MemoizedProduct = memo<typeof Product>(Product, areProductsEqual);
 
 export default Product;
